3690ae85fe4682717a4a28fa2ff93fbf39dcf21f
Kazik <kazik@Kaziks-MacBook-Pro.local>
2019-06-25 23:53:44 +0200

    create footer

--- footer.php
+++ footer.php
@@ -1,16 +1,22 @@
-<!DOCTYPE html>
-
-    <head>
-    <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    </head>
-    <body class="text-center">
-    <div class="jumbotron text-center" style="margin-bottom:0">
-  <p>Footer</p>
-</div>
-    </body>
-</html>
+<style>footer {
+    background-color: #2d2d30;
+    color: #f5f5f5;
+    padding: 32px;
+  }
+  footer a {
+    color: #f5f5f5;
+  }
+  footer a:hover {
+    color: #777;
+    text-decoration: none;
+  }
+  </style>
+<body>
+<!-- Footer -->
+<footer class="text-center">
+  <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a><br><br>
+  <p><a href="index.php" data-toggle="tooltip" title="Visit w3schools">Aplikacja ZOO</a></p> 
+</footer>
+</body>
